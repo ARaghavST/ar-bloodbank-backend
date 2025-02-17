d625a5e1732fa05d5e0d770e1d03131bf3e162eb
AnandP201 <pie1002anand@gmail.com>
2025-02-17 14:40:11 +0530

    Configured backend for global access

--- src/main/java/com/ar/bankblood/main/DonorServlet.java
+++ src/main/java/com/ar/bankblood/main/DonorServlet.java
@@ -13,69 +13,55 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-import java.io.BufferedReader;
-
-
-
-
-
 
 @WebServlet(name = "DonorServlet", urlPatterns = {"/donor"})
 public class DonorServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
+
         DatabaseConnect db = new DatabaseConnect();
-        
+
         JsonResponse res;
-            // GSON is used to convert Java objects into JSON ( Javascript ) objects 
-         Gson gson=new Gson();
-          PrintWriter out= response.getWriter();
-        MysqlConnectionObject mysql=db.ConnectAndReturnConnection();
-          
+        // GSON is used to convert Java objects into JSON ( Javascript ) objects
+        Gson gson = new Gson();
+        PrintWriter out = response.getWriter();
+        MysqlConnectionObject mysql = db.ConnectAndReturnConnection();
+
         if (mysql.connection == null) {
-            res=new JsonResponse(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,"MySQL Connection cannot be established","Connection is null",null);
-        }else{
+            res = new JsonResponse(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "MySQL Connection cannot be established", "Connection is null", null);
+        } else {
             Sqlfunction donorFunction = new Sqlfunction(mysql.connection);
-               
-        // GetDonors is returning an object of type (class) ReturnObject
-        ReturnObject ret = donorFunction.GetDonors();
-           
-        // ret.getObject returns an Object , we typecast it to List<DonorResource>
-        List<DonorResource> donorsList = (List<DonorResource>)ret.getObject();
-       
-        response.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE"); // Allowed methods
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allowed headers
-        response.setContentType("application/json");
-        
-        if (!ret.getError().equals("")){
-           res=new JsonResponse(HttpServletResponse.SC_BAD_REQUEST,"Cannot fetch donors",ret.getError(),null);
-        }else{
-            res=new JsonResponse(HttpServletResponse.SC_OK,"Donors fetched successfully",ret.getError(),donorsList);
+
+            // GetDonors is returning an object of type (class) ReturnObject
+            ReturnObject ret = donorFunction.GetDonors();
+
+            // ret.getObject returns an Object , we typecast it to List<DonorResource>
+            List<DonorResource> donorsList = (List<DonorResource>) ret.getObject();
+
+            if (!ret.getError().equals("")) {
+                res = new JsonResponse(HttpServletResponse.SC_BAD_REQUEST, "Cannot fetch donors", ret.getError(), null);
+            } else {
+                res = new JsonResponse(HttpServletResponse.SC_OK, "Donors fetched successfully", ret.getError(), donorsList);
+            }
         }
-        }
- 
+
         String resJon = gson.toJson(res);
         out.println(resJon);
-        out.flush();       
+        out.flush();
     }
 
-  
     // Below doPost will be used for signup of new user
-  
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-       
-        // We need to take the input (body) from request  
+
+        // We need to take the input (body) from request
         // We need convert it into string
-        
-        
         // this below lines from 61 -> 67 converts the json body sent from (signup form) request body into stringbuilder
         StringBuilder sb = new StringBuilder();
         try (BufferedReader reader = request.getReader()) {
@@ -84,47 +70,45 @@ public class DonorServlet extends HttpServlet {
                 sb.append(line);
             }
         }
-        
+
         // this converts stringbuilder into JAVA string
         String requestBody = sb.toString();
-        
+
         JsonResponse res;
-        // "gson" object converts JAVA string into Java object 
-        Gson gson=new Gson();
-        
+        // "gson" object converts JAVA string into Java object
+        Gson gson = new Gson();
+
         // user is POJO (Plain Old Java Object)
         // fromJson function converts string into Java objects
         UserResource user = gson.fromJson(requestBody, UserResource.class);
-        
+
         DatabaseConnect db = new DatabaseConnect();
-        
-        MysqlConnectionObject mysql=db.ConnectAndReturnConnection();
-          
-        if (mysql.connection==null){
-             res=new JsonResponse(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,"MySQL Connection cannot be established","Connection is null",null);
-        }else{
+
+        MysqlConnectionObject mysql = db.ConnectAndReturnConnection();
+
+        if (mysql.connection == null) {
+            res = new JsonResponse(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "MySQL Connection cannot be established", "Connection is null", null);
+        } else {
             Sqlfunction donorFunction = new Sqlfunction(mysql.connection);
             ReturnObject ret = donorFunction.AddNewUserMySQL(user);
-            
-                 if(!ret.getError().equals("")){
-                res=new JsonResponse(HttpServletResponse.SC_BAD_REQUEST,"Cannot insert new user",ret.getError(),null);
-            }else{
-                res=new JsonResponse(HttpServletResponse.SC_OK,"User inserted successfully",ret.getError(),null);
+
+            if (!ret.getError().equals("")) {
+                res = new JsonResponse(HttpServletResponse.SC_BAD_REQUEST, "Cannot insert new user", ret.getError(), null);
+            } else {
+                res = new JsonResponse(HttpServletResponse.SC_OK, "User inserted successfully", ret.getError(), null);
             }
-            
+
         }
-       
+
         response.setContentType("application/json");
 
         // Write JSON response to output stream
         PrintWriter out = response.getWriter();
-        
+
         String resJson = gson.toJson(res);
         out.println(resJson);
         out.flush();
-        
-        
+
     }
-    
 
 }
--- src/main/java/com/ar/bloodbank/connections/DatabaseConnect.java
+++ src/main/java/com/ar/bloodbank/connections/DatabaseConnect.java
@@ -1,51 +1,52 @@
 package com.ar.bloodbank.connections;
 
+import com.ar.bloodbank.constants.*;
+import io.github.cdimascio.dotenv.Dotenv;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import io.github.cdimascio.dotenv.Dotenv;
-import com.ar.bloodbank.constants.*;
-
 
 public class DatabaseConnect {
-    
-    private static  String URL,USER,PASSWORD,DATABASE;
-    
-    public DatabaseConnect(){
+
+    private static String URL, USER, PASSWORD, DATABASE;
+
+    public DatabaseConnect() {
         Dotenv dotenv = Dotenv.load();
-        
+
         String host = dotenv.get("MYSQL_HOST");
         String database = dotenv.get("MYSQL_DB");
-        URL = String.format("jdbc:mysql://%s:3306/%s",host,database);
-        
+        URL = String.format("jdbc:mysql://%s:3306/%s", host, database);
+
         USER = dotenv.get("MYSQL_USERNAME");
         PASSWORD = dotenv.get("MYSQL_PASSWORD");
-    };
-    
-    
-    
-    public MysqlConnectionObject ConnectAndReturnConnection(){
-         Connection connection = null;
-         
+    }
+
+    ;
+
+
+
+    public MysqlConnectionObject ConnectAndReturnConnection() {
+        Connection connection = null;
+
         try {
             // getting Driver into memory
             Class.forName("com.mysql.cj.jdbc.Driver");
 
             // connection to mysql is opening
             connection = DriverManager.getConnection(URL, USER, PASSWORD);
-
+            System.out.println(connection.getClientInfo());
             if (connection != null) {
                 // connection is done successfully
                 System.out.println("Connected to the database!");
             }
         } catch (ClassNotFoundException e) {
             System.out.println("MySQL JDBC Driver not found.");
-            return new MysqlConnectionObject(null,e.getMessage());
+            return new MysqlConnectionObject(null, e.getMessage());
         } catch (SQLException e) {
             System.out.println("Connection failed!");
-            return new MysqlConnectionObject(null,e.getMessage());
+            return new MysqlConnectionObject(null, e.getMessage());
         }
-        
-        return new MysqlConnectionObject(connection,"");
+
+        return new MysqlConnectionObject(connection, "");
     }
 }
